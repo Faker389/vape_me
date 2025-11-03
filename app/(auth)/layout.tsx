fe8564857ac65480edd81f46d87b6800c37b75a0
Faker389 <michal200614@gmail.com>
2025-11-03 23:09:45 +0100

    Fixed force-dynamic

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,5 @@
 // app/layout.tsx
+import { Suspense } from "react"
 import "../../app/globals.css"
 
 export const metadata = {
@@ -9,7 +10,12 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="font-sans antialiased">{children}</body>
+      <body className="font-sans antialiased">
+      <Suspense fallback={<div>Loading...</div>}>
+
+        {children}
+        </Suspense>
+        </body>
     </html>
   )
 }
