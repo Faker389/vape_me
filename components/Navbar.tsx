de9a4068103523789a2891e8c2e7911b48892c75
Faker389 <michal200614@gmail.com>
2026-03-13 22:07:27 +0100

    fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client"
-import "../../app/globals.css";
+import "../app/globals.css";
 import { useState, useEffect } from "react"
 import Link from "next/link"
 import Image from "next/image"
